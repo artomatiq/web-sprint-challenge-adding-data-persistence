9e2b672b4475ee7be39fccb7ea130379102b4259
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-13 18:02:58 -0500

    server working

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
-// build your server here and require it from index.js
+const express = require("express");
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/projects', projectRouter)
+
+server.use('*', (req, res, next) => {
+    res.status(404).json('not found')
+})
+
+server.use( (error, req, res, next) => {
+    res.status(error.status || 500).json({
+        message: error.message,
+        customMessage: 'error with server',
+        stack: error.stack
+    })
+})
+
+module.exports = server
