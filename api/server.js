d1148fd59b7c26b46097dc0c91f9a0e047ab49ae
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-13 19:10:54 -0500

    migration built

--- api/server.js
+++ api/server.js
@@ -8,6 +8,8 @@ const server = express()
 server.use(express.json())
 
 server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
 
 server.use('*', (req, res, next) => {
     res.status(404).json('not found')
